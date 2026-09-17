8d57df439f53ca9071b23126c18bfc44cd94274c
Shajidaa <shajidaakterlopa101@gmail.com>
2026-09-17 09:59:10 +0600

    feat: initail prisma and connected with database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from "./app";
 import config from "./app/config";
+import { prisma } from "./app/libs/prisma";
 
 const PORT = config.port;
 
 const main = async () => {
   try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
